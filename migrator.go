08829c092d7e0bea9b9c36f328d39590f1f82efc
Nick Constantine <nick@damnfine.media>
2025-07-11 23:20:41 -0400

    feat(migrator): Added Up and Down functions. Removed loadMigrations step from consumer. Added helpers for migrations.

--- migrator.go
+++ migrator.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -78,14 +79,87 @@ func WithSeedsDir(dir string) Option {
 		m.seedDir = dir
 	}
 }
+
 func WithSeedsTable(table string) Option {
 	return func(m *Migrator) {
 		m.seedTable = table
 	}
 }
 
-func (m *Migrator) LoadMigrations() error {
-	m.migrations = make([]Migration, 0)
+func (m *Migrator) Up() error {
+	err := m.loadMigrations()
+	if err != nil {
+		return fmt.Errorf("Could not load migrations %w", err)
+	}
+	appliedMigrations, err := m.fetchAppliedMigrations()
+	if err != nil {
+		return fmt.Errorf("Could not fetch applied migrations %w", err)
+	}
+	err = m.validateMigrations(m.migrations)
+	if err != nil {
+		return fmt.Errorf("Could not validate migrations %w", err)
+	}
+	var pendingMigrations []Migration
+	for _, migration := range m.migrations {
+		if !appliedMigrations[migration.version] {
+			pendingMigrations = append(pendingMigrations, migration)
+		}
+	}
+	if len(pendingMigrations) == 0 {
+		fmt.Println("No migrations to apply")
+		return nil
+	}
+
+	for _, migration := range pendingMigrations {
+		err = m.executeMigration(migration, false)
+		if err != nil {
+			return fmt.Errorf("Could not execute migration %d: %w", migration.version, err)
+		}
+	}
+
+	return nil
+}
+
+func (m *Migrator) Down(targetVersion int) error {
+	err := m.loadMigrations()
+	if err != nil {
+		return fmt.Errorf("Could not load migrations %w", err)
+	}
+	appliedMigrations, err := m.fetchAppliedMigrations()
+	if err != nil {
+		return fmt.Errorf("Could not fetch applied migrations %w", err)
+	}
+	err = m.validateMigrations(m.migrations)
+	if err != nil {
+		return fmt.Errorf("Could not validate migrations %w", err)
+	}
+	var pendingMigrations []Migration
+	for _, migration := range m.migrations {
+		if appliedMigrations[migration.version] && migration.version > targetVersion {
+			pendingMigrations = append(pendingMigrations, migration)
+		}
+	}
+	if len(pendingMigrations) == 0 {
+		fmt.Println("No migrations to rollback")
+		return nil
+	}
+
+	sort.Slice(pendingMigrations, func(i, j int) bool {
+		return pendingMigrations[i].version > pendingMigrations[j].version
+	})
+
+	for _, migration := range pendingMigrations {
+		err = m.executeMigration(migration, true)
+		if err != nil {
+			return fmt.Errorf("Could not execute rollback %d: %w", migration.version, err)
+		}
+	}
+
+	return nil
+}
+
+func (m *Migrator) loadMigrations() error {
+	migrations := []Migration{}
 	fmt.Println("Loading migrations...")
 
 	//appliedMigrations, err := m.fetchAppliedMigrations()
@@ -118,7 +192,7 @@ func (m *Migrator) LoadMigrations() error {
 		}
 		up, down, err := m.parseFile(migFile)
 		if err != nil {
-			message := fmt.Sprintf("is not a valid version number")
+			message := fmt.Sprintf("is not a valid version number %w", err)
 			return errors.New(message)
 		}
 		migration := Migration{
@@ -128,20 +202,34 @@ func (m *Migrator) LoadMigrations() error {
 			up:       up,
 			down:     down,
 		}
-		fmt.Println("Mig file up", migration.up)
-		m.migrations = append(m.migrations, migration)
+
+		migrations = append(migrations, migration)
 	}
 
+	sort.Slice(migrations, func(i, j int) bool {
+		return migrations[i].version < migrations[j].version
+	})
+	m.migrations = migrations
+
 	return nil
 }
 
-func (m *Migrator) LoadSeeds() error {
-	m.seeds = make([]Seed, 0)
+func (m *Migrator) validateMigrations(migrations []Migration) error {
+	for i, migration := range migrations {
+		expectedVersion := i + 1
+		if migration.version != expectedVersion {
+			if migration.version < expectedVersion {
+				return fmt.Errorf("Duplicate migration version. Expected %d, got %d", migration.version, expectedVersion)
+			} else {
+				return fmt.Errorf("Migration Gap detected. Expected %d, got %d", expectedVersion, migration.version)
+			}
+		}
+	}
 	return nil
 }
 
-func (m *Migrator) fetchAppliedMigrations() ([]Migration, error) {
-	appliedMigrations := []Migration{}
+func (m *Migrator) fetchAppliedMigrations() (map[int]bool, error) {
+	appliedMigrations := map[int]bool{}
 
 	query := fmt.Sprintf("SELECT * FROM %s ORDER BY version ASC", m.migrationTable)
 	result, err := m.db.Query(query)
@@ -155,7 +243,7 @@ func (m *Migrator) fetchAppliedMigrations() ([]Migration, error) {
 		if err != nil {
 			return nil, err
 		}
-		appliedMigrations = append(appliedMigrations, migration)
+		appliedMigrations[migration.version] = true
 	}
 	if err := result.Err(); err != nil {
 		return nil, err
@@ -182,4 +270,67 @@ func (m *Migrator) parseFile(file os.DirEntry) (string, string, error) {
 	}
 
 	return up, down, nil
+}
+
+func (m *Migrator) splitSQL(sql string) []string {
+	commands := strings.Split(sql, ",")
+	var statements []string
+	for _, command := range commands {
+		trimmedCommand := strings.TrimSpace(command)
+		if trimmedCommand == "" || strings.HasPrefix(trimmedCommand, "--") {
+			continue
+		}
+		if !strings.HasSuffix(trimmedCommand, ";") {
+			trimmedCommand += ";"
+		}
+		statements = append(statements, trimmedCommand)
+	}
+	return statements
+}
+
+func (m *Migrator) executeMigration(migration Migration, isDown bool) error {
+	fmt.Printf("Executing migration: %d %s\n", migration.version, migration.name)
+	var statements []string
+	if isDown {
+		statements = m.splitSQL(migration.down)
+	} else {
+		statements = m.splitSQL(migration.up)
+	}
+
+	transaction, err := m.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer transaction.Rollback()
+	for i, statement := range statements {
+		fmt.Printf("Executing statement: %d/%d\n", i+1, len(statements))
+		if _, err := transaction.Exec(statement); err != nil {
+			return fmt.Errorf("Error executing statement: %d/%d for migration %d %w\n", i, len(statements), migration.version, err)
+		}
+	}
+	if isDown {
+		query := fmt.Sprintf("DELETE FROM %S WHERE VERSION = %d;", m.migrationTable, migration.version)
+		_, err = transaction.Exec(query)
+		if err != nil {
+			return fmt.Errorf("failed to record down migrations: %w", err)
+		}
+	} else {
+		query := fmt.Sprintf("INSERT INTO %S (version, name, applied_at) VALUES ($1, $2, now());", m.migrationTable)
+		_, err = transaction.Exec(query, migration.version, migration.name)
+		if err != nil {
+			return fmt.Errorf("failed to record up migrations: %w", err)
+		}
+	}
+	err = transaction.Commit()
+	if err != nil {
+		return fmt.Errorf("failed to commit migrations: %w", err)
+	}
+	fmt.Printf("✅ Migration %d: %s succeeded!\n", migration.version, migration.name)
+
+	return nil
+}
+
+func (m *Migrator) loadSeeds() error {
+	m.seeds = []Seed{}
+	return nil
 }
